3398d6194fd8232490a27913d10546a20b28b7a7
Isabella Rovigati <56564990+isabellasrovigati@users.noreply.github.com>
2020-07-17 17:12:15 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Estilização from './pages/Estilização/index';
-import Counter from './pages/Contador/index.js';
 
 ReactDOM.render(
   <React.StrictMode>
